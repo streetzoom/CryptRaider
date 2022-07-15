d1550419e98686896a07b280d3e3c20a6df56f1f
Ilya Bril <brilillja81@gmail.com>
2022-07-15 20:48:08 +0300

    Actor Tags
    Added and setup Actor Tags to unlock Secret door only if specific actor overlap

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -3,10 +3,39 @@
 
 #include "TriggerComponent.h"
 
+// Sets default values for this component's properties
+UTriggerComponent::UTriggerComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
 // Called when the game starts
 void UTriggerComponent::BeginPlay()
 {
 	Super::BeginPlay();
+}
 
-	UE_LOG(LogTemp, Display, TEXT("Trigger Component Alive:"));
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);	
+}
+
+AActor* UTriggerComponent::GetAcceptableActor() const
+{
+	TArray<AActor*> Actors;
+	GetOverlappingActors(Actors);
+
+	for (auto Actor : Actors)
+	{
+		if (Actor->ActorHasTag(AcceptableActorTag))
+		{
+			return Actor;
+		}		
+	}
+	
+	return  nullptr;
 }
--- Source/CryptRaider/TriggerComponent.h
+++ Source/CryptRaider/TriggerComponent.h
@@ -4,17 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "Components/BoxComponent.h"
+
 #include "TriggerComponent.generated.h"
 
-/**
- * 
- */
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDER_API UTriggerComponent : public UBoxComponent
 {
 	GENERATED_BODY()
+public:
+	UTriggerComponent();
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere)
+	FName AcceptableActorTag;
 
+	AActor* GetAcceptableActor() const;
 };
